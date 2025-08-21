e0f265ef23b6697e63879d925ed2275654fb321d
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-08-21 16:31:18 +0300

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {Box, Grid, GridItem} from "@chakra-ui/react";
+import {NavBar} from "@/components/NavBar.tsx";
 
 function App() {
 
@@ -7,7 +8,9 @@ function App() {
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
     }}>
-      <GridItem area='nav' bg={'coral'}>Nav</GridItem>
+      <GridItem area='nav'>
+        <NavBar />
+      </GridItem>
       <Box display={{ base: 'none', lg: 'block' }}>
         <GridItem area='aside' bg={'gold'}>
           Aside
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from "@/components/ui/provider"
-// import './index.css'
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,5 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <Provider>
       <App />
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
